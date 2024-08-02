7ad61d2bfc0cca04d1da2c886ddbcf8e0eef06be
PopsAlex <alexpops3000@gmail.com>
2024-08-03 00:05:41 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#1st program
+print(9 ** 0.5 * 5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+a = 1234
+b = 5678
+c = a % 1000
+d = c // 10
+f = b % 1000
+e = f // 10
+print(d + e)
+#4th program
+aa = 13.42
+bb = 42.13
+cc = int(aa // 1)
+dd = int(aa * 100 % 1000 % 100)
+ff = int(bb // 1)
+ee = int(bb * 100 % 1000 % 100)
+print(cc == ee or dd == ff)
